fa83cd8f8993bbe40cf1a53ce2ae9fbc00253af2
Ryan Christian <33403762+rschristian@users.noreply.github.com>
2024-07-10 17:10:15 -0500

    chore: Add some types to router, rename a couple vars (#32)

---
+++ src/internal.d.ts
@@ -0,0 +1,15 @@
+export interface AugmentedComponent extends Component<any, any> {
+	__v: VNode;
+	__c: (error: Promise<void>, suspendingVNode: VNode) => void;
+}
+
+export interface VNode<P = any> extends preact.VNode<P> {
+	__c: AugmentedComponent;
+	__e?: Element | Text;
+	__u: number;
+	__h: boolean;
+	__v?: VNode<P>;
+	__k: Array<VNode<any>> | null;
+}
+
+export {}
