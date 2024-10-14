cfa6b82d7d6ed19cff5f5c4e22a9d60466647786
Ryan Christian <rchristian@ryanchristian.dev>
2024-10-14 16:58:16 -0500

    fix: Ensure isodata script tag will pass spec validators

--- src/hydrate.js
+++ src/hydrate.js
@@ -5,7 +5,7 @@ let initialized;
 /** @type {typeof hydrativeRender} */
 export function hydrate(jsx, parent) {
 	if (typeof window === 'undefined') return;
-	let isodata = document.querySelector('script[type=isodata]');
+	let isodata = document.getElementById('isodata');
 	// @ts-ignore-next
 	parent = parent || (isodata && isodata.parentNode) || document.body;
 	if (!initialized && isodata) {
