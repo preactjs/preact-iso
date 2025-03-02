da4f077be2741337a57267d33d3ef46e769c92d6
Ryan Christian <rchristian@ryanchristian.dev>
2025-03-01 18:14:42 -0600

    test: Progress, though still incomplete

--- test/router.test.js
+++ test/router.test.js
@@ -50,7 +50,8 @@ describe('Router', () => {
 			scratch
 		);
 
-		loc.route('/a/');
+		navigation.navigate('/a/');
+
 		await sleep(1);
 
 		expect(loc).to.deep.include({
@@ -154,7 +155,7 @@ describe('Router', () => {
 		});
 
 		Home.resetHistory();
-		loc.route('/profiles');
+		navigation.navigate('/profiles');
 		await sleep(1);
 
 		expect(scratch).to.have.property('textContent', 'Profiles');
@@ -170,7 +171,7 @@ describe('Router', () => {
 		});
 
 		Profiles.resetHistory();
-		loc.route('/profiles/bob');
+		navigation.navigate('/profiles/bob');
 		await sleep(1);
 
 		expect(scratch).to.have.property('textContent', 'Profile: bob');
@@ -188,7 +189,7 @@ describe('Router', () => {
 		});
 
 		Profile.resetHistory();
-		loc.route('/other?a=b&c=d');
+		navigation.navigate('/other?a=b&c=d');
 		await sleep(1);
 
 		expect(scratch).to.have.property('textContent', 'Fallback');
@@ -242,7 +243,7 @@ describe('Router', () => {
 		expect(A).to.have.been.calledWith({ path: '/', searchParams: {}, pathParams: {} });
 
 		A.resetHistory();
-		loc.route('/b');
+		navigation.navigate('/b');
 
 		expect(scratch).to.have.property('innerHTML', '<h1>A</h1><p>hello</p>');
 		expect(A).not.to.have.been.called;
@@ -263,18 +264,18 @@ describe('Router', () => {
 		expect(B).to.have.been.calledWith({ path: '/b', searchParams: {}, pathParams: {} });
 
 		B.resetHistory();
-		loc.route('/c');
-		loc.route('/c?1');
-		loc.route('/c');
+		navigation.navigate('/c');
+		navigation.navigate('/c?1');
+		navigation.navigate('/c');
 
 		expect(scratch).to.have.property('innerHTML', '<h1>B</h1><p>hello</p>');
 		expect(B).not.to.have.been.called;
 
 		await sleep(1);
 
-		loc.route('/c');
-		loc.route('/c?2');
-		loc.route('/c');
+		navigation.navigate('/c');
+		navigation.navigate('/c?2');
+		navigation.navigate('/c');
 
 		expect(scratch).to.have.property('innerHTML', '<h1>B</h1><p>hello</p>');
 		// We should never re-invoke <B /> while loading <C /> (that would be a remount of the old route):
@@ -293,7 +294,7 @@ describe('Router', () => {
 
 		C.resetHistory();
 		B.resetHistory();
-		loc.route('/b');
+		navigation.navigate('/b');
 		await sleep(1);
 
 		expect(scratch).to.have.property('innerHTML', '<h1>B</h1><p>hello</p>');
@@ -303,7 +304,7 @@ describe('Router', () => {
 
 		A.resetHistory();
 		B.resetHistory();
-		loc.route('/');
+		navigation.navigate('/');
 		await sleep(1);
 
 		expect(scratch).to.have.property('innerHTML', '<h1>A</h1><p>hello</p>');
@@ -347,21 +348,21 @@ describe('Router', () => {
 		expect(renderRefCount).to.equal(2);
 
 		renderRefCount = 0;
-		loc.route('/b/a');
+		navigation.navigate('/b/a');
 		await sleep(10);
 
 		expect(scratch).to.have.property('innerHTML', '<h1>b/a</h1>');
 		expect(renderRefCount).to.equal(4);
 
 		renderRefCount = 0;
-		loc.route('/b/b');
+		navigation.navigate('/b/b');
 		await sleep(10);
 
 		expect(scratch).to.have.property('innerHTML', '<h1>b/b</h1>');
 		expect(renderRefCount).to.equal(1);
 
 		renderRefCount = 0;
-		loc.route('/');
+		navigation.navigate('/');
 		await sleep(10);
 
 		expect(scratch).to.have.property('innerHTML', '<h1>a</h1>');
@@ -451,7 +452,8 @@ describe('Router', () => {
 		loadEnd.resetHistory();
 		routeChange.resetHistory();
 
-		loc.route('/b');
+		navigation.navigate('/b');
+
 		await sleep(1);
 
 		expect(loadStart).to.have.been.calledWith('/b');
@@ -508,7 +510,7 @@ describe('Router', () => {
 		expect(loadEnd).not.to.have.been.called;
 	});
 
-	describe('intercepted VS external links', () => {
+	describe.only('intercepted VS external links', () => {
 		const shouldIntercept = [null, '', '_self', 'self', '_SELF'];
 		const shouldNavigate = ['_top', '_parent', '_blank', 'custom', '_BLANK'];
 
@@ -588,8 +590,6 @@ describe('Router', () => {
 		const shouldIntercept = ['/app', '/app/deeper'];
 		const shouldNavigate = ['/site', '/site/deeper'];
 
-		const clickHandler = sinon.fake(e => e.preventDefault());
-
 		const Links = () => (
 			<>
 				<a href="/app">Internal Link</a>
@@ -598,23 +598,6 @@ describe('Router', () => {
 				<a href="/site/deeper">External Deeper Link</a>
 			</>
 		);
-
-		let pushState;
-
-		before(() => {
-			pushState = sinon.spy(history, 'pushState');
-			addEventListener('click', clickHandler);
-		});
-
-		after(() => {
-			pushState.restore();
-			removeEventListener('click', clickHandler);
-		});
-
-		beforeEach(async () => {
-			clickHandler.resetHistory();
-			pushState.resetHistory();
-		});
 
 		it('should intercept clicks on links matching the `scope` props (string)', async () => {
 			render(
@@ -629,15 +612,10 @@ describe('Router', () => {
 				scratch.querySelector(`a[href="${url}"]`).click();
 				await sleep(1);
 				expect(loc).to.deep.include({ url });
-				expect(pushState).to.have.been.calledWith(null, '', url);
-				expect(clickHandler).to.have.been.called;
-
-				pushState.resetHistory();
-				clickHandler.resetHistory();
 			}
 		});
 
-		it('should allow default browser navigation for links not matching the `scope` props (string)', async () => {
+		it.skip('should allow default browser navigation for links not matching the `scope` props (string)', async () => {
 			render(
 				<LocationProvider scope="app">
 					<Links />
@@ -649,11 +627,8 @@ describe('Router', () => {
 			for (const url of shouldNavigate) {
 				scratch.querySelector(`a[href="${url}"]`).click();
 				await sleep(1);
-				expect(pushState).not.to.have.been.called;
-				expect(clickHandler).to.have.been.called;
 
-				pushState.resetHistory();
-				clickHandler.resetHistory();
+				// TODO: How to test this?
 			}
 		});
 
@@ -670,15 +645,10 @@ describe('Router', () => {
 				scratch.querySelector(`a[href="${url}"]`).click();
 				await sleep(1);
 				expect(loc).to.deep.include({ url });
-				expect(pushState).to.have.been.calledWith(null, '', url);
-				expect(clickHandler).to.have.been.called;
-
-				pushState.resetHistory();
-				clickHandler.resetHistory();
 			}
 		});
 
-		it('should allow default browser navigation for links not matching the `scope` props (regex)', async () => {
+		it.skip('should allow default browser navigation for links not matching the `scope` props (regex)', async () => {
 			render(
 				<LocationProvider scope={/^\/app/}>
 					<Links />
@@ -690,11 +660,8 @@ describe('Router', () => {
 			for (const url of shouldNavigate) {
 				scratch.querySelector(`a[href="${url}"]`).click();
 				await sleep(1);
-				expect(pushState).not.to.have.been.called;
-				expect(clickHandler).to.have.been.called;
 
-				pushState.resetHistory();
-				clickHandler.resetHistory();
+				// TODO: How to test this?
 			}
 		});
 	});
@@ -702,7 +669,14 @@ describe('Router', () => {
 	it('should scroll to top when navigating forward', async () => {
 		const scrollTo = sinon.spy(window, 'scrollTo');
 
-		const Route = sinon.fake(() => <div style={{ height: '1000px' }}><a href="/link">link</a></div>);
+		const Route = sinon.fake(
+			() => (
+				<div style={{ height: '1000px' }}>
+					<a href="/link">link</a>
+				</div>
+			)
+		);
+
 		render(
 			<LocationProvider>
 				<Router>
@@ -717,7 +691,7 @@ describe('Router', () => {
 		expect(Route).to.have.been.calledOnce;
 		Route.resetHistory();
 
-		loc.route('/programmatic');
+		navigation.navigate('/programmatic');
 		await sleep(1);
 
 		expect(loc).to.deep.include({ url: '/programmatic' });
@@ -740,14 +714,13 @@ describe('Router', () => {
 	});
 
 	it('should ignore clicks on document fragment links', async () => {
-		const pushState = sinon.spy(history, 'pushState');
-
 		const Route = sinon.fake(
 			() => <div>
 				<a href="#foo">just #foo</a>
 				<a href="/other#bar">other #bar</a>
 			</div>
 		);
+
 		render(
 			<LocationProvider>
 				<Router>
@@ -760,7 +733,6 @@ describe('Router', () => {
 			scratch
 		);
 
-		expect(Route).to.have.been.calledOnce;
 		Route.resetHistory();
 
 		scratch.querySelector('a[href="#foo"]').click();
@@ -769,7 +741,6 @@ describe('Router', () => {
 		// NOTE: we don't (currently) propagate in-page anchor navigations into context, to avoid useless renders.
 		expect(loc).to.deep.include({ url: '/' });
 		expect(Route).not.to.have.been.called;
-		expect(pushState).not.to.have.been.called;
 		expect(location.hash).to.equal('#foo');
 
 		scratch.querySelector('a[href="/other#bar"]').click();
@@ -777,14 +748,10 @@ describe('Router', () => {
 
 		expect(Route).to.have.been.calledOnce;
 		expect(loc).to.deep.include({ url: '/other#bar', path: '/other' });
-		expect(pushState).to.have.been.called;
 		expect(location.hash).to.equal('#bar');
-
-		pushState.restore();
 	});
 
 	it('should normalize children', async () => {
-		const pushState = sinon.spy(history, 'pushState');
 		const Route = sinon.fake(() => <a href="/foo#foo">foo</a>);
 
 		const routes = ['/foo', '/bar'];
@@ -807,9 +774,6 @@ describe('Router', () => {
 
 		expect(Route).to.have.been.calledOnce;
 		expect(loc).to.deep.include({ url: '/foo#foo', path: '/foo' });
-		expect(pushState).to.have.been.called;
-
-		pushState.restore();
 	});
 
 	it('should match nested routes', async () => {
@@ -865,25 +829,30 @@ describe('Router', () => {
 	});
 
 	it('should replace the current URL', async () => {
-		const pushState = sinon.spy(history, 'pushState');
-		const replaceState = sinon.spy(history, 'replaceState');
-
 		render(
 			<LocationProvider>
 				<Router>
+					<Route path="/" component={() => null} />
 					<Route path="/foo" component={() => null} />
+					<Route path="/bar" component={() => null} />
 				</Router>
 				<ShallowLocation />
 			</LocationProvider>,
 			scratch
 		);
 
-		loc.route("/foo", true);
-		expect(pushState).not.to.have.been.called;
-		expect(replaceState).to.have.been.calledWith(null, "", "/foo");
+		navigation.navigate('/foo');
+		navigation.navigate('/bar', { history: 'replace' });
 
-		pushState.restore();
-		replaceState.restore();
+		const entries = navigation.entries();
+
+		// Top of the stack
+		const last = new URL(entries[entries.length - 1].url);
+		expect(last.pathname).to.equal('/bar');
+
+		// Entry before
+		const secondLast = new URL(entries[entries.length - 2].url);
+		expect(secondLast.pathname).to.equal('/');
 	});
 });
 
